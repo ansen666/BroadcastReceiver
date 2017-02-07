f1934d7c0026fdaf1aaed72eee86f2acc7eb3f91
ansen <anhui@365yf.com>
2017-02-07 18:11:31 +0800

    增加LocalBroadcastManager

--- app/src/main/java/com/ansen/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/ansen/broadcastreceiver/MainActivity.java
@@ -2,6 +2,7 @@ package com.ansen.broadcastreceiver;
 
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,8 +10,12 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     public static final String ACTION_DYNAMIC_BROADCAST="android.intent.action.DYNAMIC_BROADCAST";
+    public static final String ACTION_LOCAL_BROADCAST="android.intent.action.LOCAL_BROADCAST";
 
     private DynamicBroadcast dynamicBroadcast;
+
+    private LocalBroadcastManager broadcastManager;
+    private LocalBroadcastReceiver localReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findViewById(R.id.btn_dynamic_broadcast_send_message).setOnClickListener(this);
         findViewById(R.id.btn_static_broadcast_send_message).setOnClickListener(this);
+        findViewById(R.id.btn_local_broadcast_send_message).setOnClickListener(this);
 
         //动态注册广播
         dynamicBroadcast=new DynamicBroadcast();
         IntentFilter intentFilter=new IntentFilter(ACTION_DYNAMIC_BROADCAST);
         registerReceiver(dynamicBroadcast,intentFilter);
+
+        //使用LocalBroadcastManager注册广播
+        broadcastManager = LocalBroadcastManager.getInstance(this);
+        localReceiver=new LocalBroadcastReceiver();
+        broadcastManager.registerReceiver(localReceiver,new IntentFilter(ACTION_LOCAL_BROADCAST));
     }
 
     @Override
@@ -39,6 +50,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 staticIntent.putExtra("data","Static Broadcast Parameter");//通过intent传参
                 sendBroadcast(staticIntent);//发送广播消息
                 break;
+            case R.id.btn_local_broadcast_send_message://发送本地广播
+                Intent localIntent=new Intent(ACTION_LOCAL_BROADCAST);
+                localIntent.putExtra("data","Local Broadcast Parameter");//通过intent传参
+                LocalBroadcastManager.getInstance(this).sendBroadcast(localIntent);
+                break;
         }
     }
 
@@ -47,5 +63,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onDestroy();
         Log.i("MainActivity onDestroy","销毁广播");
         unregisterReceiver(dynamicBroadcast);
+        broadcastManager.unregisterReceiver(localReceiver);
     }
 }
